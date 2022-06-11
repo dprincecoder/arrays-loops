7ca88af7ef29842a3660c544ef9ab325cb5ac741
dprincecoder <dprincecoder@gmail.com>
2022-06-11 19:48:18 +0100

    first commit

---
+++ TODO.js
@@ -0,0 +1,56 @@
+/*this challenge should you choose to accept it:
+attempt at least 2 out of 3*/
+
+//use this log where ncessary and when you want to log something:
+const { log } = console;
+
+/*--==============================-----------------------=======================-----------------------=======================*/
+
+//array 1
+const arrOfNum = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+
+//1. Write a function that takes an array of numbers and returns the sum of all the numbers.
+
+//2. Write a function that takes an array and return the total items in the array.
+
+//3. write a function that takes an array of numbers and returns a number you want from the array.
+
+/*--==============================-----------------------=======================-----------------------=======================*/
+
+//array 2
+const arrOfName = ["Peter", "Paul", "Mary", "John", "Jane"];
+
+//1. Write a function that takes an array of names and returns the first name and last name.
+
+//2. write a function that takes an array of names and return the name that is 5 letters long.
+
+//3. write a function that takes an array of names and returns (your name) at the end of the array.
+
+/*--==============================-----------------------=======================-----------------------=======================*/
+
+//array 3
+const arrOfCountry = ["USA", "UK", "Germany", "France", "Nigeria"];
+
+//1. Write a function that takes an array of countries and returns the country that starts with the letter 'N'.
+
+//2. Write a function that takes an array of countries and returns the country that ends with the letter 'A'.
+
+//3. Write a function that takes an array of countries and returns the country that has the letter 'a' in the middle.
+
+/*--==============================-----------------------=======================-----------------------=======================*/
+
+//array 4
+const arrOfObject = [
+  { name: "Peter", age: 30, country: "USA" },
+  { name: "Paul", age: 25, country: "UK" },
+  { name: "Mary", age: 28, country: "Germany" },
+  { name: "John", age: 35, country: "France" },
+  { name: "Jane", age: 28, country: "Nigeria" },
+];
+
+//1 write a function that takes an array of objects then loop through the array in 3 types of looping an array of objects.
+//this 3 methods must be in 3 different functions.
+
+//2 write a function that takes an array of objects and returns the object that has age that is greater than 20 and less than 30.
+
+//3 write a function that takes an array of objects and returns the object that has name that is 'John'.
