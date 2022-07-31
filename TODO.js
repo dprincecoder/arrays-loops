27750a4895fd5f510ff1905734fc7d5fe92959c3
Avus <avuwake@gmail.com>
2022-08-01 00:14:03 +0100

    Optimizations

--- TODO.js
+++ TODO.js
@@ -1,72 +1,71 @@
-/*this challenge should you choose to accept it:
-attempt at least 2 out of 3*/
+//This challenge, should you choose to accept: You must attempt at least 2 out of 3.
 
-//use this log where ncessary and when you want to log something:
+//Use this log where necessary & when you want to log result:
 const { log } = console;
 
 
-//use this html to test your code in the browser:
-const sumOfNumber = document.querySelector(".sum-of-no");
-const lengthOfArr = document.querySelector(".len-of-arr");
+//Use the html selectors to display your code on the browser accordingly.
+
+const sumOfAllNumbers = document.querySelector(".sum-of-all-nums");
 const totalItemsOfArr = document.querySelector(".len-of-arr");
-const NumOfChoice = document.querySelector(".num-of-choice");
+const numOfChoice = document.querySelector(".num-of-choice");
 const firstAndLastName = document.querySelector(".first-and-last-name");
-const fiveLetterLong = document.querySelector(".five-letters-long");
+const fiveLettersLong = document.querySelector(".five-letters-long");
 const countryWithAatTheEnd = document.querySelector(".country-with-a-at-end");
-const countryStartingWithN = document.querySelector(".country-with-starting-with-n");
-const countryWithAInMiddle = document.querySelector(".country-with-a-in-middle");
+const countryStartingWithN = document.querySelector(".country-starting-with-n");
+const countryWithAInbetween = document.querySelector(".country-with-a-inbetween");
 
 
 /*--==============================-----------------------=======================-----------------------=======================*/
 
-//hint: to insert your result in the html, use the textContent property of the element you want to insert the result in.
-//e.g: sumOfNumber.textContent = yourResult;
+//Hint: to insert your result in the html, use the 'textContent' property on the element you want to insert the result.
+//e.g: sumOfAllNumbers.textContent = result();
 
 //array 1
-const arrOfNum = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+const arrOfNums = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
 
-//1. Write a function that takes an array of numbers and returns the sum of all the numbers.
+//1. Write a function that takes an array of numbers & return the sum of all the numbers.
 
-//2. Write a function that takes an array of numbers and return the total items in the array.
+//2. Write a function that takes an array of numbers & return the total items in the array.
 
-//3. write a function that takes an array of numbers and returns a number you want from the array.
+//3. Write a function that takes an array of numbers & returns a number you want from the array.
 
 /*--==============================-----------------------=======================-----------------------=======================*/
 
 //array 2
-const arrOfName = ["Peter", "Paul", "Mary", "John", "Jane"];
+const arrOfNames = ["Peter", "Paul", "Mary", "John", "Jane"];
 
-//1. Write a function that takes an array of names and returns the first name and last name.
+//1. Write a function that takes an array of names & return the first & last name in the array.
 
-//2. write a function that takes an array of names and return the name that is 5 letters long.
+//2. write a function that takes an array of names & return names that are 5 letters long.
 
-//3. write a function that takes an array of names and returns (your name) at the end of the array.
+//3. Write a function that takes an array of names & return (your name) at the end of the array.
 
 /*--==============================-----------------------=======================-----------------------=======================*/
 
 //array 3
-const arrOfCountry = ["USA", "UK", "Germany", "France", "Nigeria"];
+const arrOfCountries = ["USA", "UK", "Germany", "France", "Nigeria"];
 
-//1. Write a function that takes an array of countries and returns the country that starts with the letter 'N'.
+//1. Write a function that takes an array of countries & return the country that starts with the letter 'n'.
 
-//2. Write a function that takes an array of countries and returns the country that ends with the letter 'A'.
+//2. Write a function that takes an array of countries & return the country that ends with the letter 'a'.
 
-//3. Write a function that takes an array of countries and returns the country that has the letter 'a' in the middle.
+//3. Write a function that takes an array of countries & return the country that has the letter 'a' inbetween.
 
 /*--==============================-----------------------=======================-----------------------=======================*/
 
 //array 4
-const arrOfObject = [
+const arrOfObjects = [
   { name: "Peter", age: 30, country: "USA" },
-  { name: "Paul", age: 25, country: "UK" },
+  { name: "Paul", age: 25, country: "England" },
   { name: "Mary", age: 28, country: "Germany" },
   { name: "John", age: 35, country: "France" },
   { name: "Jane", age: 28, country: "Nigeria" },
 ];
 
-//1 write a function that takes an array of objects then loop through the array in 3 types of looping an array of objects.
-//this 3 methods must be in 3 different functions.
+//1. Write a function that takes an array of objects. Use 3 types of looping methods on it.
+//1..The 3 methods must be in 3 different functions.
 
-//2 write a function that takes an array of objects and returns the object that has age that is greater than 20 and less than 30.
+//2 Write a function that takes an array of objects & returns the object with age greater than 20 & less than 30.
 
-//3 write a function that takes an array of objects and returns the object that has name that is 'John'.
+//3 Write a function that takes an array of objects & returns the object that has the name 'John'.
--- avos.js
+++ avos.js
@@ -1,181 +1,181 @@
+'use strict'
 //use this html to test your code in the browser:
-const sumOfNumber = document.querySelector(".sum-of-no");
-const lengthOfArr = document.querySelector(".len-of-arr");
+const sumOfAllNumbers = document.querySelector(".sum-of-all-nums");
 const totalItemsOfArr = document.querySelector(".len-of-arr");
-const NumOfChoice = document.querySelector(".num-of-choice");
+const numOfChoice = document.querySelector(".num-of-choice");
 const firstAndLastName = document.querySelector(".first-and-last-name");
-const fiveLetterLong = document.querySelector(".five-letters-long");
+const fiveLettersLong = document.querySelector(".five-letters-long");
 const countryWithAatTheEnd = document.querySelector(".country-with-a-at-end");
-const countryStartingWithN = document.querySelector(".country-with-starting-with-n");
-const countryWithAInMiddle = document.querySelector(".country-with-a-in-middle");
+const countryStartingWithN = document.querySelector(".country-starting-with-n");
+const countryWithAInbetween = document.querySelector(".country-with-a-inbetween");
 
 //array 1
-const arrOfNum = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+const arrOfNums = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
 
 //Task 1.1
 const accumulator = arr => {
     let result = 0
-    for(let item of arr){
+    for (let item of arr) {
         result += item
     }
     return result
 }
-sumOfNumber.textContent = accumulator(arrOfNum)
+sumOfAllNumbers.textContent = accumulator(arrOfNums)
 
 //Task 1.2
-const totalLength = arr => {
+const totalItems = arr => {
     let result
-    for(let item of arr){
-       result = item 
+    for (let item of arr) {
+        result = item
     }
     return result
 }
-lengthOfArr.textContent = totalLength(arrOfNum)
+totalItemsOfArr.textContent = totalItems(arrOfNums)
 
 //Task 1.3
 const chosenNum = (arr, num) => {
     let result
-    for(let item of arr){
-        if(num === item) result = item
-     }
-     return result
+    for (let item of arr) {
+        if (num === item) result = item
+    }
+    return result
 }
-NumOfChoice.textContent = chosenNum(arrOfNum, 2)
+numOfChoice.textContent = chosenNum(arrOfNums, 2)
 
 //array 2
-const arrOfName = ["Peter", "Paul", "Mary", "John", "Jane"];
+const arrOfNames = ["Peter", "Paul", "Mary", "John", "Jane"];
 
 //Task 2.1
 const fnlName = (arr) => {
     let result = []
-    for(let item of arr){
-        if(item === arr[0] || item === arr[arr.length - 1]) {
+    for (let item of arr) {
+        if (item === arr[0] || item === arr[arr.length - 1]) {
             result.push(item)
         }
-     }
-     return result
+    }
+    return result
 }
-firstAndLastName.textContent = fnlName(arrOfName)
+firstAndLastName.textContent = fnlName(arrOfNames)
 
 //Task 2.2
 const fiveLetters = (arr) => {
     let result = []
-    for(let item of arr){
-        if(item.length === 5) {
+    for (let item of arr) {
+        if (item.length === 5) {
             result.push(item)
         }
-     }
-     return result
+    }
+    return result
 }
-fiveLetterLong.textContent = fiveLetters(arrOfName)
+fiveLettersLong.textContent = fiveLetters(arrOfNames)
 
 //Task 2.3
 const myName = (arr, newName) => {
     arr.push(newName)
-    return {arr, newName}
+    return { arr, newName }
 }
-console.log(myName(arrOfName, 'Avos'))
+console.log(myName(arrOfNames, 'Avos'))
 
 //array 3
-const arrOfCountry = ["USA", "UK", "Germany", "France", "Nigeria"];
+const arrOfCountries = ["USA", "UK", "Germany", "France", "Nigeria"];
 
 //Task 3.1
 const startsWithN = (arr) => {
     let result = []
-    for(let item of arr){
-        if(item[0] === 'N') {
+    for (let item of arr) {
+        if (item[0] === 'N') {
             result.push(item)
         }
-     }
-     return result
+    }
+    return result
 }
-countryStartingWithN.textContent = startsWithN(arrOfCountry)
+countryStartingWithN.textContent = startsWithN(arrOfCountries)
 
 //Task 3.2
-const EndsWithA = (arr) => {
+const endsWithA = (arr) => {
     let result = []
-    for(let item of arr){
-        if(item[item.length - 1] === 'A') {
+    for (let item of arr) {
+        if (item[item.length - 1] === 'A') {
             result.push(item)
         }
-     }
-     return result
+    }
+    return result
 }
-countryWithAatTheEnd.textContent = EndsWithA(arrOfCountry)
+countryWithAatTheEnd.textContent = endsWithA(arrOfCountries)
 
 //Task 3.2
-const middleLetterA = (arr) => {
+const aInbetween = (arr) => {
     let result = []
-    for(let item of arr){
-        let sliced = item.slice(1,[item.length - 1])
-        for(let i of sliced){ 
-            if(i === 'a'){
+    for (let item of arr) {
+        let sliced = item.slice(1, [item.length - 1])
+        for (let i of sliced) {
+            if (i === 'a') {
                 result.push(item)
             }
         }
-     }
-     return result
- }
-countryWithAInMiddle.textContent = middleLetterA(arrOfCountry)
+    }
+    return result
+}
+countryWithAInbetween.textContent = aInbetween(arrOfCountries)
 
 //array 4
-const arrOfObject = [
+const arrOfObjects = [
     { name: "Peter", age: 30, country: "USA" },
-    { name: "Paul", age: 25, country: "UK" },
+    { name: "Paul", age: 25, country: "England" },
     { name: "Mary", age: 28, country: "Germany" },
     { name: "John", age: 35, country: "France" },
     { name: "Jane", age: 28, country: "Nigeria" },
-  ];
+];
 
 //Task 4.1.1
 const loopObjOne = arr => {
     let result = []
-    for(let item of arr){
+    for (let item of arr) {
         result.push(item)
     }
     return result
 }
-console.log(loopObjOne(arrOfObject))
+console.log(loopObjOne(arrOfObjects))
 
 //Task 4.1.2
 const loopObjTwo = arr => {
     let result = []
     let obj = 0
-    while(obj < arr.length){
+    while (obj < arr.length) {
         result.push(arr[obj])
         obj++
     }
     return result
 }
-console.log(loopObjTwo(arrOfObject))
+console.log(loopObjTwo(arrOfObjects))
 
 //Task 4.1.3
 const loopObjTree = arr => {
     let result = []
     let obj = 0
-    do{
+    do {
         result.push(arr[obj])
         obj++
     }
-    while(obj < arr.length) 
+    while (obj < arr.length)
     return result
 }
-console.log(loopObjTree(arrOfObject))
+console.log(loopObjTree(arrOfObjects))
 
 //Task 4.2
 const ageSort = arr => {
-    result = arr.filter(arr => arr.age > 20 && arr.age < 30)
+    let result = arr.filter(arr => arr.age > 20 && arr.age < 30)
     return result
 }
-console.log(ageSort(arrOfObject))
+console.log(ageSort(arrOfObjects))
 
 //Task 4.3
 const nameChecker = (arr, name) => {
     let result = []
-    for(let item of arr){ 
-        if(name ===  item.name)
-        result.push(item)
+    for (let item of arr) {
+        if (name === item.name)
+            result.push(item)
     }
     return result
 }
-console.log(nameChecker(arrOfObject, 'John'))
+console.log(nameChecker(arrOfObjects, 'John'))
